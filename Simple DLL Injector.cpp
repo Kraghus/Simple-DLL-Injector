a156e3a1bf22908a0b1b736751765e37aa26aede
Kraghus <44181473+Kraghus@users.noreply.github.com>
2023-10-22 05:39:55 -0400

    Added Function
    Added function to get the process ID of the target process.

--- Simple DLL Injector.cpp
+++ Simple DLL Injector.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <windows.h>
+#include <TlHelp32.h>
+
+DWORD GetProcessID(const char* processName)
+{
+    DWORD processID = 0;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 processEntry;
+        processEntry.dwSize = sizeof(processEntry);
+
+        if (Process32First(hSnapshot, &processEntry))
+        {
+            do
+            {
+                if (!_stricmp(processEntry.szExeFile, processName))
+                {
+                    processID = processEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnapshot, &processEntry));
+        }
+    }
+    CloseHandle(hSnapshot);
+    return processID;
+}
 
 int main()
 {
